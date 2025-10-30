85acd9402a940819e3f84faa2bd03997a111d532
Ting Chien Meng <tcm390@nyu.edu>
2025-10-30 20:44:58 +0800

    yeild history

--- src/plugins/plugin-defillama/src/index.ts
+++ src/plugins/plugin-defillama/src/index.ts
@@ -2,17 +2,18 @@ import type { Plugin } from "@elizaos/core";
 import { DefiLlamaService } from "./services/defillama.service";
 import { getProtocolTvlAction } from "./actions/getProtocolTvl.action";
 import { getYieldRatesAction } from "./actions/getYieldRates.action";
+import { getYieldHistoryAction } from "./actions/getYieldHistory.action";
 
 export const defiLlamaPlugin: Plugin = {
   name: "plugin-defillama",
-  description: "DeFiLlama integration: protocol TVL lookups and yield opportunities",
-  actions: [getProtocolTvlAction, getYieldRatesAction],
+  description: "DeFiLlama integration: protocol TVL lookups, yield opportunities, and historical trends",
+  actions: [getProtocolTvlAction, getYieldRatesAction, getYieldHistoryAction],
   evaluators: [],
   providers: [],
   services: [DefiLlamaService],
 };
 
 export default defiLlamaPlugin;
-export { DefiLlamaService, getProtocolTvlAction, getYieldRatesAction };
+export { DefiLlamaService, getProtocolTvlAction, getYieldRatesAction, getYieldHistoryAction };
 
 
